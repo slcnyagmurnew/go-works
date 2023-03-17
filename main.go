4af0256ab7382436c670805e01c37226d97508c0
Selcan Yağmur Atak <l1119601@std.yildiz.edu.tr>
2023-03-17 17:51:20 +0300

    feat(main): add created functions to main

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"exampleProject/functions"
 	"exampleProject/pkg"
+	"exampleProject/types"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	types.CreateSlices()
+	types.CopyData()
+	types.GetSliceData()
+	types.MapOperations()
+	types.IterateWithRanges()
+	types.CreateArray()
+
+	functions.CallMultipleReturns()
+
 	router := gin.Default()
 	router.GET("/locations/init", pkg.InitLocations)
 	router.GET("/locations/get", pkg.GetLocations)
 	router.POST("/locations/append", pkg.AddLocation)
-	router.Run("localhost:8000")
+	_ = router.Run("localhost:8000")
 
 }
